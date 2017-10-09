3040632f502dd665f75e40b66bc9646804b47775
padlabear <padlabearel@gmail.com>
2017-10-10 00:17:28 +0300

    - Create Calculator - Coverage test with Mockito

---
+++ calculator/src/main/java/com/example/calculator/CalculatorActivity.java
@@ -0,0 +1,70 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CalculatorActivity extends AppCompatActivity {
+    private EditText inputFieldEditText;
+    private TextView resultTextView;
+    private Button calculateButton;
+    private Calculator calculator = new DefaultCalculator();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+        initView();
+    }
+
+
+    private void initView() {
+        inputFieldEditText = (EditText) findViewById(R.id.input_editText);
+        resultTextView = (TextView) findViewById(R.id.result_textView);
+        calculateButton = (Button) findViewById(R.id.calculate_button);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String result = calculator.evaluate(inputFieldEditText.getText().toString());
+                showResult(result);
+            }
+        });
+
+        inputFieldEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int length = editable.length();
+                if (length > 0) {
+                    calculateButton.setEnabled(true);
+                } else {
+                    calculateButton.setEnabled(false);
+                }
+            }
+        });
+
+
+    }
+
+
+    private void showResult(String result) {
+        resultTextView.setText(result);
+    }
+
+
+}
