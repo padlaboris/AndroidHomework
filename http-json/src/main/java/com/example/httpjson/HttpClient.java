fe07822943c7f525caea6d77a9e7a2ca1f3e83c5
padlabear <padlabearel@gmail.com>
2017-10-14 23:51:25 +0300

    Create new empty module

---
+++ http-json/src/main/java/com/example/httpjson/HttpClient.java
@@ -0,0 +1,13 @@
+package com.example.httpjson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HttpClient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_client);
+    }
+}
